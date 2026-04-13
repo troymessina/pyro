21c4e3f2fe74fea54940dd975e1ef3b9d051a24f
Troy Messina <troychristophermessina@gmail.com>
2026-04-13 09:48:15 -0400

    Add clockface cylinders in clockface-cylinders.py

---
+++ src/examples/introtext/clockface-cylinders.py
@@ -0,0 +1,16 @@
+from vpython import *
+L=10
+R = 0.1
+
+cyl1 = cylinder(pos=vec(0,0,0), axis=vec(1,0,0), radius=R, length=L, color=color.magenta)
+cyl2 = cylinder(pos=vec(0,0,0), axis=vec(cos(pi/6),sin(pi/6),0), radius=R, length=L, color=color.magenta)
+cyl3 = cylinder(pos=vec(0,0,0), axis=vec(cos(2*pi/6),sin(2*pi/6),0), radius=R, length=L, color=color.magenta)
+cyl4 = cylinder(pos=vec(0,0,0), axis=vec(cos(3*pi/6),sin(3*pi/6),0), radius=R, length=L, color=color.magenta)
+cyl5 = cylinder(pos=vec(0,0,0), axis=vec(cos(4*pi/6),sin(4*pi/6),0), radius=R, length=L, color=color.magenta)
+cyl6 = cylinder(pos=vec(0,0,0), axis=vec(cos(5*pi/6),sin(5*pi/6),0), radius=R, length=L, color=color.magenta)
+cyl7 = cylinder(pos=vec(0,0,0), axis=vec(cos(6*pi/6),sin(6*pi/6),0), radius=R, length=L, color=color.magenta)
+cyl8 = cylinder(pos=vec(0,0,0), axis=vec(cos(7*pi/6),sin(7*pi/6),0), radius=R, length=L, color=color.magenta)
+cyl9 = cylinder(pos=vec(0,0,0), axis=vec(cos(8*pi/6),sin(8*pi/6),0), radius=R, length=L, color=color.magenta)
+cyl10 = cylinder(pos=vec(0,0,0), axis=vec(cos(9*pi/6),sin(9*pi/6),0), radius=R, length=L, color=color.magenta)
+cyl11 = cylinder(pos=vec(0,0,0), axis=vec(cos(10*pi/6),sin(10*pi/6),0), radius=R, length=L, color=color.magenta)
+cyl12 = cylinder(pos=vec(0,0,0), axis=vec(cos(11*pi/6),sin(11*pi/6),0), radius=R, length=L, color=color.magenta)
