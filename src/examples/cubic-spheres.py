976d9f7efa0f14320081a70c014d50440db845a4
Troy Messina <troychristophermessina@gmail.com>
2026-04-14 11:01:04 -0400

    Add cubic spheres visualization in cubic-spheres.py

---
+++ src/examples/cubic-spheres.py
@@ -0,0 +1,12 @@
+from vpython import *
+a = 0.5
+r = 0.5
+
+s1 = sphere(pos=vec(a,a,a), radius=r, color=color.green)
+s2 = sphere(pos=vec(-a,a,a), radius=r, color=color.red)
+s3 = sphere(pos=vec(a,-a,a), radius=r, color=color.orange)
+s4 = sphere(pos=vec(a,a,-a), radius=r, color=color.magenta)
+s5 = sphere(pos=vec(-a,-a,a), radius=r, color=color.blue)
+s6 = sphere(pos=vec(-a,a,-a), radius=r, color=color.orange)
+s7 = sphere(pos=vec(a,-a,-a), radius=r, color=color.cyan)
+s8 = sphere(pos=vec(-a,-a,-a), radius=r, color=color.yellow)
